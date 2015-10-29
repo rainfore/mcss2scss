40d0c566e3b31c3cc18f4dd3291d60bcd6f6f92f
Yusen <rainforest92@126.com>
2015-10-29 17:20:27 +0800

    [added] gulp

---
+++ src/gulp.js
@@ -0,0 +1,30 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var mcss2scss = require('./index.js');
+
+var PluginError = gutil.PluginError;
+
+var extend = function(o1, o2, override) {
+    for(var i in o2)
+        if(override || o1[i] === undefined)
+            o1[i] = o2[i];
+
+    return o1;
+}
+
+module.exports = function (opt) {
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) return cb(null, file); 
+        if (file.isStream()) return cb(new PluginError('gulp-mcss2scss', 'Streaming not supported'));
+
+        // var options = extend(opt || {}, {
+        //     filename: file.path
+        // });
+        var str = file.contents.toString();
+        var dest = mcss2scss(str);
+
+        file.contents = new Buffer(dest);
+        file.path = file.path.replace(/\.mcss$/, '.scss');
+        cb(null, file);
+    });
+};
---
+++ test/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var mcss2scss = require('../src/gulp.js');
+
+gulp.task('mcss2scss', function() {
+    return gulp.src('./var.mcss')
+    .pipe(mcss2scss())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['mcss2scss']);
